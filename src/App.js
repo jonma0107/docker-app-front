2e4411263e06ef9d27e246f3fdd0b35cd52cf6f2
Jonathan Meza <practicantesena3@ticsocial.com.co>
2023-02-20 17:30:25 -0500

    POST Method

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ class App extends React.Component {
     this.state = {
       todoList: [],
       activeItem: {
-        id: null,
         title: '',
         description: '',
         state: '',
         priority: '',
-        // completed: false,
+        deliver_date: '',
+        comment: '',
+        
       },
-      // editing:false,
+      
     }
     this.fetchTasks = this.fetchTasks.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleSelectChange = this.handleSelectChange.bind(this)
-  };
+    this.handleDateChange = this.handleDateChange.bind(this)
+  }; // Constructor
 
   componentWillMount(){
     this.fetchTasks()
@@ -30,7 +32,6 @@ class App extends React.Component {
     console.log('Fetching...');
 
     fetch('http://localhost:8000/api/list/')
-    // fetch('http://localhost:8000/api/task-list/')
     .then(res => res.json())
     .then(data =>
       this.setState({
@@ -38,6 +39,8 @@ class App extends React.Component {
       })
     )
   }
+
+  // *********************************************************  State String's **********************************************
 
   handleChange(e){
     let name = e.target.name
@@ -50,26 +53,65 @@ class App extends React.Component {
         ...this.state.activeItem,
         title:value,
         description:value,
-        state:value,
-        // priority:value
+        comment:value,
       }
     })
   }
 
-  handleSelectChange = (event) => {
-    const value = event.target.value;
-    this.setState(prevState => ({
-      activeItem: {
-        ...prevState.activeItem,
-        priority:value
+  // ********************************************************** State Select 1 ***************************************************
+
+  handleSelectChange2 = (e) => {
+    let name = e.target.name
+    let value = e.target.value
+    console.log('Name:', name);
+    console.log('Value:', value);
+    
+    this.setState({
+      activeItem:{
+        ...this.state.activeItem,
+        state:value,        
       }
-    }));
-  }  
+    });
+  }
+
+  // ********************************************************** State Select 2 ***************************************************
+
+  handleSelectChange = (e) => {
+    let name = e.target.name
+    let value = e.target.value
+    console.log('Name:', name);
+    console.log('Value:', value);
+    
+    this.setState({
+      activeItem:{
+        ...this.state.activeItem,
+        priority:value,        
+      }
+    });
+  }
+
+  // ********************************************************** State Input Date ***************************************************
+  
+  handleDateChange = (deliver_date) => {
+    let name = deliver_date.target.name
+    let value = deliver_date.target.value
+    console.log('Name:', name);
+    console.log('Value:', value);
+
+    this.setState({
+      activeItem:{
+        ...this.state.activeItem,
+        deliver_date: value,        
+      }
+    });
+  }    
+
+  // ********************************************************** State Submit *******************************************************
 
   handleSubmit(e){
     e.preventDefault()
     console.log('ITEM:', this.state.activeItem);
-
+    
     let url = 'http://localhost:8000/api/list/'
     fetch(url, {
       method: 'POST',
@@ -81,18 +123,30 @@ class App extends React.Component {
       this.fetchTasks()
       this.setState({
         activeItem:{
-          id:null,
           title:'',
           description:'',
           state:'',
           priority:'',
-          // completed:false
+          deliver_date: '',
+          comment: '',
+          
         }
       })
     }).catch(function(error) {
       console.log('ERROR:', error);
     })
+
+    // borrarForm()
+
   }
+  
+  // function borrarForm() {
+    
+  // }
+
+  // *********************************************************************************************************************************
+  // *******************************************************  RENDER  ****************************************************************
+  // *********************************************************************************************************************************
 
   render(){
     let tasks = this.state.todoList
@@ -104,16 +158,22 @@ class App extends React.Component {
             <form onSubmit={this.handleSubmit} id='form'>
               <div className='flex-wrapper'>
 
+                {/* Title */}
+
                 <div style={{flex:6}}>
                   <input onChange={this.handleChange} className='form-control' id='title' type="text" name='title' placeholder='add task name'/>
                 </div>
 
-                <div style={{flex:6}}>
-                  <input onChange={this.handleChange} className='form-control' id='title' type="text" name='description' placeholder='add a description'/>
-                </div>
+                {/* Description */}
 
                 <div style={{flex:6}}>
-                  <select value={this.state.activeItem.state} onChange={this.handleChange}>
+                  <textarea name='description' className='form-control' onChange={this.handleChange} id='title' ></textarea>
+                </div>
+
+                {/* Select State */}
+
+                <div style={{flex:6}}>
+                  <select value={this.state.activeItem.state} onChange={this.handleSelectChange2} name="state">
                     <option value="BACKLOG">BACKLOG</option>
                     <option value="TO DO">TO DO</option>
                     <option value="DOING">DOING</option>
@@ -122,14 +182,30 @@ class App extends React.Component {
                   </select>
                 </div>
 
+                {/* Select Priority */}
+
                 <div style={{flex:6}}>
-                  <select value={this.state.activeItem.priority} onChange={this.handleSelectChange}>
+                  <select value={this.state.activeItem.priority} onChange={this.handleSelectChange} name='priority'>
                     <option value="HIGH">HIGH</option>
                     <option value="MEDIUM">MEDIUM</option>
                     <option value="LOW">LOW</option>
                     
                   </select>
                 </div>
+
+                {/* Deliver_Date */}
+
+                <div style={{flex:6}}>
+                  <input name='deliver_date' className='form-control' type='date' onChange={this.handleDateChange}/>                  
+                </div>
+
+                {/* Comment */}
+
+                <div style={{flex:6}}>
+                  <textarea name='comment' className='form-control' onChange={this.handleChange} id='title' ></textarea>
+                </div>
+
+                {/* SUBMIT */}
 
                 <div style={{flex: 1}}>
                   <input id='submit' className='btn btn-warning' type="submit" name='add'/>
@@ -138,13 +214,14 @@ class App extends React.Component {
             </form>
           </div>
 
+          {/* LIST */}
+
           <div id='list-wrapper'> 
             {tasks.map(function(task, index){
               return(
                 <div key={index} className='task-wrapper flex-wrapper'>
                   <div style={{flex:7}}>
 
-                    {/* <span>{task.title}</span>                    */}
                     <p>{task.title}</p>
                     <p>{task.description}</p>
                     <p>{task.state}</p>
@@ -156,10 +233,14 @@ class App extends React.Component {
                     
                   </div>
 
+                  {/* uPDATE */}
+
                   <div style={{flex:1}}>
                   <button className='btn btn-sm btn-outline-info'>Edit</button>
                     
                   </div>
+
+                  {/* dELETE */}
 
                   <div style={{flex:1}}>
                   <button className='btn btn-sm btn-outline-danger delete'>Delete</button>
@@ -177,7 +258,7 @@ class App extends React.Component {
       </div>
     )
   }
-}
+} // Fin Clase App
 
 
 export default App;
